ffa1629816b2ef6c554b0cdd55c625663f1adc43
Daniel McNerthney <dan.mcnerthney@softllc.com>
2011-09-29 11:15:27 -0700

    add title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -12,7 +12,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => "Ruby on Rails Sample App | Home")
      end
      it "show have a non-blank body" do
        get 'home'
@@ -29,7 +29,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+                        :content => "Ruby on Rails Sample App | Contact")
      end
   end
 
@@ -41,7 +41,7 @@ describe PagesController do
      it "should have the right title" do
        get 'about'
        response.should have_selector("title",
-                         :content => "Ruby on Rails Tutorial Sample App | About")
+                         :content => "Ruby on Rails Sample App | About")
       end
    end
 
